230ad6b10fce26e3fbcbce65a9133e191a2a1040
guaru345 <abelmtz34@outlook.com>
2023-08-22 15:04:39 -0600

    fix problem with esmodules socket.io need a commonjs modules

--- index.js
+++ index.js
@@ -1,78 +1,77 @@
-import express from "express";
-import http from "http"
-import cors from "cors"
-import os from "os"
-import {Server} from "socket.io"
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const os = require("os");
+const { Server } = require("socket.io");
 
-const app = express()
-const server = http.createServer(app)
-const io = new Server(server)
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.use(cors())
-app.use(express.static('public'));
+app.use(cors());
 
-let players = []
+let players = [];
 
-io.on('connection',(socket) => {
-    console.log(`usuario conectado ${socket.id}`)
+io.on("connection", (socket) => {
+  console.log(`usuario conectado ${socket.id}`);
 
-    players.push(socket)
+  players.push(socket);
+  console.log(players.length);
+  if (players.length === 2) {
+    io.emit("game_start");
+  }
 
-    if(players.length === 2){
-        io.emit('game_start')
+  socket.on("choice", (choice) => {
+    socket.choice = choice;
+
+    if (players.every((player) => player.choice !== undefined)) {
+      compareChoices();
     }
+  });
 
-    socket.on('choice', (choice) => {
-        socket.choice = choice;
-        
-        if (players.every(player => player.choice !== undefined)) {
-          compareChoices();
-        }
-    });
-
-    socket.on('disconnect',() => {
-        console.log(`Usuario desconectado: ${socket.id}`);
-        players = players.filter(player => player !== socket);
-    })
-})
+  socket.on("disconnect", () => {
+    console.log(`Usuario desconectado: ${socket.id}`);
+    players = players.filter((player) => player !== socket);
+  });
+});
 
 function compareChoices() {
-    const choice1 = players[0].choice;
-    const choice2 = players[1].choice;
-    
-    let result;
-    if (choice1 === choice2) {
-      result = 'Empate';
-    } else if (
-      (choice1 === 'piedra' && choice2 === 'tijeras') ||
-      (choice1 === 'tijeras' && choice2 === 'papel') ||
-      (choice1 === 'papel' && choice2 === 'piedra')
-    ) {
-      result = 'Jugador 1 gana';
-    } else {
-      result = 'Jugador 2 gana';
-    }
-    
-    players[0].emit('game_result', result);
-    players[1].emit('game_result', result);
+  const choice1 = players[0].choice;
+  const choice2 = players[1].choice;
+
+  let result;
+  if (choice1 === choice2) {
+    result = "Empate";
+  } else if (
+    (choice1 === "piedra" && choice2 === "tijeras") ||
+    (choice1 === "tijeras" && choice2 === "papel") ||
+    (choice1 === "papel" && choice2 === "piedra")
+  ) {
+    result = "Jugador 1 gana";
+  } else {
+    result = "Jugador 2 gana";
+  }
+
+  players[0].emit("game_result", result);
+  players[1].emit("game_result", result);
 }
 
-const IP_ADDRESS = getIPAddress()
-const PORT = process.env.PORT || 1234
+const IP_ADDRESS = getIPAddress();
+const PORT = process.env.PORT || 4000;
 
 function getIPAddress() {
-    const interfaces = os.networkInterfaces();
-    for (const interfaceName in interfaces) {
-        const interfaceData = interfaces[interfaceName];
-        for (const data of interfaceData) {
-            if (data.family === 'IPv4' && !data.internal) {
-                return data.address;
-            }
-        }
+  const interfaces = os.networkInterfaces();
+  for (const interfaceName in interfaces) {
+    const interfaceData = interfaces[interfaceName];
+    for (const data of interfaceData) {
+      if (data.family === "IPv4" && !data.internal) {
+        return data.address;
+      }
     }
-    return '127.0.0.1'; // Dirección IP por defecto si no se puede detectar automáticamente
+  }
+  return "127.0.0.1"; // Dirección IP por defecto si no se puede detectar automáticamente
 }
 
-app.listen(PORT,() => {
-    console.log(`server listening on http://${IP_ADDRESS}:${PORT}`)
-})
+server.listen(PORT, () => {
+  console.log(`server listening on ${PORT}`);
+});
